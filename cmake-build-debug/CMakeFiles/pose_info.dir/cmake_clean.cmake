e4c7f801dbf83bbcbf07c87fb712d5761e078d13
kmolan <anmolkathail@gmail.com>
2019-11-21 17:16:03 -0500

    cleanup

---
+++ cmake-build-debug/CMakeFiles/pose_info.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pose_info.dir/src/pose_info.cpp.o"
+  "devel/lib/pose_info/pose_info"
+  "devel/lib/pose_info/pose_info.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose_info.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
